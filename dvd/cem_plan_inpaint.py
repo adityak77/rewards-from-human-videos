70dc48517c2f6dbe3ea2b221a836731fefecbf4a
Aditya Kannan <akannan2@andrew.cmu.edu>
2022-11-26 12:35:34 -0500

    inpaint cem open loop simulation sampling script

---
+++ dvd/cem_plan_inpaint.py
@@ -0,0 +1,235 @@
+import matplotlib
+matplotlib.use('Agg')
+import os
+import numpy as np
+from torch import nn as nn
+import torch
+from torch.distributions.multivariate_normal import MultivariateNormal
+import time
+import copy
+
+import argparse
+import logging
+
+from sim_env.tabletop import Tabletop
+from optimizer_utils import CemLogger, decode_gif
+from optimizer_utils import load_discriminator_model, load_encoder_model, dvd_reward
+from optimizer_utils import reward_push_mug_left_to_right, reward_push_mug_forward, reward_close_drawer, tabletop_obs
+# from optimizer_utils import vip_reward, vip_reward_trajectory_similarity
+
+from inpaint_utils import get_human_cfg, get_robot_cfg, get_segmentation_model, get_inpaint_model, inpaint
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)s %(asctime)s %(pathname)s:%(lineno)d] %(message)s',
+                    datefmt='%m-%d %H:%M:%S')
+logging.getLogger('matplotlib.font_manager').disabled = True
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task_id", type=int, required=True, help="Task to learn a model for")
+
+# optimizer specific
+parser.add_argument("--learn_dynamics_model", action='store_true', default=False, help='Learn a dynamics model (otherwise use online sampling)')
+parser.add_argument("--engineered_rewards", action='store_true', default=False, help='Use hand engineered rewards or not')
+parser.add_argument("--dvd", action='store_true', help='Use dvd rewards')
+parser.add_argument("--vip", action='store_true', help='Use pretrained VIP embeddings for reward function')
+
+# for DVD or VIP rewards
+parser.add_argument("--demo_path", type=str, default=None, help='path to demo video')
+
+# dvd model params
+parser.add_argument("--checkpoint", type=str, default='test/tasks6_seed0_lr0.01_sim_pre_hum54144469394_dem60_rob54193/model/150sim_discriminator.pth.tar', help='path to model')
+parser.add_argument('--similarity', action='store_true', default=True, help='whether to use similarity discriminator') # needs to be true for MultiColumn init
+parser.add_argument('--hidden_size', type=int, default=512, help='latent encoding size')
+parser.add_argument('--num_tasks', type=int, default=2, help='number of tasks') # needs to exist for MultiColumn init
+
+# env initialization
+parser.add_argument("--log_dir", type=str, default="mppi")
+parser.add_argument("--env_log_freq", type=int, default=20) 
+parser.add_argument("--verbose", type=bool, default=1) 
+parser.add_argument("--xml", type=str, default='env1')
+
+# E2FGVI args
+parser.add_argument("-c", "--ckpt", type=str, default='/home/akannan2/inpainting/E2FGVI/release_model/E2FGVI-CVPR22.pth')
+parser.add_argument("--model", type=str, default='e2fgvi', choices=['e2fgvi', 'e2fgvi_hq'])
+parser.add_argument("--step", type=int, default=10)
+parser.add_argument("--num_ref", type=int, default=-1)
+parser.add_argument("--neighbor_stride", type=int, default=1)
+parser.add_argument("--savefps", type=int, default=24)
+
+# args for e2fgvi_hq (which can handle videos with arbitrary resolution)
+parser.add_argument("--set_size", action='store_true', default=False)
+parser.add_argument("--width", type=int)
+parser.add_argument("--height", type=int)
+
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    TIMESTEPS = 51 # MPC lookahead - max length of episode
+    N_SAMPLES = 100
+    NUM_ELITES = 7
+    NUM_ITERATIONS = 100
+    nu = 4
+
+    dtype = torch.double
+
+    # only one of these reward functions allowed per run
+    assert sum([args.engineered_rewards, args.dvd, args.vip]) == 1
+
+    # load in models
+    human_cfg = get_human_cfg()
+    robot_cfg = get_robot_cfg()
+    human_segmentation_model = get_segmentation_model(human_cfg)
+    robot_segmentation_model = get_segmentation_model(robot_cfg)
+    inpaint_model = get_inpaint_model(args)
+
+    if args.engineered_rewards:
+        if args.task_id == 94:
+            terminal_reward_fn = reward_push_mug_left_to_right
+        elif args.task_id == 41:
+            terminal_reward_fn = reward_push_mug_forward
+        elif args.task_id == 5:
+            terminal_reward_fn = reward_close_drawer
+    elif args.dvd:
+        assert args.demo_path is not None
+        if args.demo_path.startswith('demos'):
+            assert args.demo_path.endswith(str(args.task_id))
+        video_encoder = load_encoder_model(args)
+        sim_discriminator = load_discriminator_model(args)
+        terminal_reward_fn = dvd_reward
+    elif args.vip:
+        assert args.demo_path is not None
+        if args.demo_path.startswith('demos'):
+            assert args.demo_path.endswith(str(args.task_id))
+        video_encoder = None
+        sim_discriminator = None
+        terminal_reward_fn = vip_reward
+
+    if not args.engineered_rewards:
+        if os.path.isdir(args.demo_path):
+            all_demo_names = os.listdir(args.demo_path)
+            demos = [decode_gif(os.path.join(args.demo_path, fname)) for fname in all_demo_names]
+        else:
+            demos = [decode_gif(args.demo_path)]
+
+        # TODO: Inpaint demos here (and maintain same formatting)
+        # import ipdb; ipdb.set_trace()
+        demos = [inpaint(args, inpaint_model, human_segmentation_model, demo) for demo in demos]
+
+    # initialization
+    actions_mean = torch.zeros(TIMESTEPS * nu)
+    actions_cov = torch.eye(TIMESTEPS * nu)
+
+    # for logging
+    if args.engineered_rewards:
+        logdir = 'engineered_reward'
+    elif args.dvd:
+        logdir = args.checkpoint.split('/')[1]
+    elif args.vip:
+        logdir = 'vip2'
+    
+    logdir = 'inpaint_' + logdir + f'_{TIMESTEPS}_{N_SAMPLES}_{NUM_ITERATIONS}_task{args.task_id}'
+    logdir = os.path.join('cem_plots', logdir)
+    run = 0
+    while os.path.isdir(logdir + f'_run{run}'):
+        run += 1
+    logdir = logdir + f'_run{run}'
+    
+    cem_logger = CemLogger(logdir)
+    for ep in range(NUM_ITERATIONS):
+        # env initialization
+        env = Tabletop(log_freq=args.env_log_freq, 
+                    filepath=args.log_dir + '/env',
+                    xml=args.xml,
+                    verbose=args.verbose)  # bypass the default TimeLimit wrapper
+        _, start_info = env.reset_model()
+        very_start = tabletop_obs(start_info)
+
+        tstart = time.perf_counter()
+        action_distribution = MultivariateNormal(actions_mean, actions_cov)
+        action_samples = action_distribution.sample((N_SAMPLES,))
+        sample_rewards = torch.zeros(N_SAMPLES)
+
+        if args.dvd or args.vip:
+            states = np.zeros((N_SAMPLES, TIMESTEPS, 120, 180, 3))
+
+        for i in range(N_SAMPLES):
+            env_copy = copy.deepcopy(env)
+            for t in range(TIMESTEPS):
+                u = action_samples[i, t*nu:(t+1)*nu]
+                obs, _, _, env_copy_info = env_copy.step(u.cpu().numpy())
+                if args.dvd or args.vip:
+                    states[i, t] = obs
+            
+            if args.engineered_rewards:
+                curr_state = tabletop_obs(env_copy_info)
+                sample_rewards[i] = terminal_reward_fn(curr_state, u, very_start=very_start)
+
+        if args.dvd or args.vip:
+            # TODO: Inpaint states here
+            # import ipdb; ipdb.set_trace()
+            states = (states * 255).astype(np.uint8)
+            states = [inpaint(args, inpaint_model, robot_segmentation_model, sample) for sample in states]
+            sample_rewards = terminal_reward_fn(states, _, demos=demos, video_encoder=video_encoder, sim_discriminator=sim_discriminator)
+
+        # update elites
+        _, best_inds = torch.topk(sample_rewards, NUM_ELITES)
+        elites = action_samples[best_inds]
+
+        actions_mean = elites.mean(dim=0)
+        actions_cov = torch.Tensor(np.cov(elites.cpu().numpy().T))
+        if torch.matrix_rank(actions_cov) < actions_cov.shape[0]:
+            actions_cov += 1e-5 * torch.eye(actions_cov.shape[0])
+
+        # follow sampled trajectory
+        action_distribution = MultivariateNormal(actions_mean, actions_cov)
+        traj_sample = action_distribution.sample((1,))
+
+        states = np.zeros((1, TIMESTEPS, 120, 180, 3))
+
+        for t in range(TIMESTEPS):
+            action = traj_sample[0, t*nu:(t+1)*nu] # from CEM
+            obs, r, done, low_dim_info = env.step(action.cpu().numpy())
+            states[0, t] = obs
+        tend = time.perf_counter()
+
+        # ALL CODE BELOW for logging sampled trajectory
+        additional_reward_type = 'vip' if args.vip else 'dvd'
+        if not args.engineered_rewards:
+            additional_reward = terminal_reward_fn(states, _, demos=demos, video_encoder=video_encoder, sim_discriminator=sim_discriminator).item()
+        else:
+            additional_reward = 0 # NA
+
+        low_dim_state = tabletop_obs(low_dim_info)
+        # TODO: fix reward section below / integrate with gt rewards above
+        if args.task_id == 94:
+            rew = low_dim_state[3] - very_start[3]
+            gt_reward = -np.abs(low_dim_state[3] - very_start[3] - 0.15) + 0.15
+            penalty = 0 if (np.abs(low_dim_state[10] - very_start[10]) < 0.03 and low_dim_state[12] < 0.01) else -100
+            success_threshold = 0.05
+        elif args.task_id == 41:
+            rew = low_dim_state[4] - very_start[4]
+            gt_reward = -np.abs(low_dim_state[4] - very_start[4] - 0.115) + 0.115
+            penalty = 0  # if (np.abs(low_dim_state[3] - very_start[3]) < 0.05) else -100
+            success_threshold = 0.03
+        elif args.task_id == 5:
+            rew = low_dim_state[10]
+            gt_reward = low_dim_state[10]
+            penalty = 0 if (np.abs(low_dim_state[3] - very_start[3]) < 0.01) else -100
+            success_threshold = -0.01
+
+        # calculate success and reward of trajectory
+        gt_reward += penalty
+        succ = gt_reward > success_threshold
+        mean_sampled_rewards = sample_rewards.cpu().numpy().mean()
+
+        logger.debug(f"{ep}: Trajectory time: {tend-tstart:.4f}\t Object Position Shift: {rew:.6f}")
+        cem_logger.update(gt_reward, succ, mean_sampled_rewards, additional_reward, additional_reward_type)
+
+        # logging results
+        all_obs = (states[0] * 255).astype(np.uint8)
+        cem_logger.save_graphs(all_obs)
---
+++ dvd/inpaint_utils.py
@@ -0,0 +1,219 @@
+import os
+import importlib
+
+import numpy as np
+import cv2
+import torch
+
+from tqdm import tqdm
+from PIL import Image
+
+import sys
+sys.path.append('/home/akannan2/inpainting/detectron2')
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultPredictor
+
+sys.path.append('/home/akannan2/inpainting/E2FGVI/')
+from core.utils import to_tensors
+
+def get_human_cfg():
+    config_file = '/home/akannan2/inpainting/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'
+    weights = 'detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl'
+    confidence_threshold = 0.5
+
+    cfg = get_cfg()
+    cfg.merge_from_file(config_file)
+
+    cfg.MODEL.WEIGHTS = weights
+    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = confidence_threshold
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = confidence_threshold
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = confidence_threshold
+    cfg.freeze()
+    return cfg
+
+def get_robot_cfg():
+    config_file = '/home/akannan2/inpainting/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'
+    weights = '/home/akannan2/inpainting/detectron2/output/model_final.pth'
+    confidence_threshold = 0.7
+    
+    cfg = get_cfg()
+    cfg.merge_from_file(config_file)
+
+    cfg.MODEL.WEIGHTS = weights
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = confidence_threshold
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+    return cfg
+
+
+def get_segmentation_model(cfg):
+    predictor = DefaultPredictor(cfg)
+    return predictor
+
+def get_inpaint_model(args):
+    # set up models
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    net = importlib.import_module('model.' + args.model)
+    model = net.InpaintGenerator().to(device)
+    data = torch.load(args.ckpt, map_location=device)
+    model.load_state_dict(data)
+    print(f'Loading model from: {args.ckpt}')
+    model.eval()
+
+    return model
+
+def get_ref_index(f, neighbor_ids, length, ref_length, num_ref):
+    ref_index = []
+    if num_ref == -1:
+        for i in range(0, length, ref_length):
+            if i not in neighbor_ids:
+                ref_index.append(i)
+    else:
+        start_idx = max(0, f - ref_length * (num_ref // 2))
+        end_idx = min(length, f + ref_length * (num_ref // 2))
+        for i in range(start_idx, end_idx + 1, ref_length):
+            if i not in neighbor_ids:
+                if len(ref_index) > num_ref:
+                    break
+                ref_index.append(i)
+    return ref_index
+
+# resize frames
+def resize_frames(frames, size=None):
+    if size is not None:
+        frames = [f.resize(size) for f in frames]
+    else:
+        size = frames[0].size
+    return frames, size
+
+def process_masks(masks, size = None):
+    masks_expanded = []
+    for mask in masks:
+        if mask.shape[0] == 0:
+            m = np.zeros(size).astype(np.uint8)
+        else:
+            m = np.clip(mask.cpu().numpy().astype(np.uint8).sum(axis=0), 0, 1)
+
+        m = Image.fromarray(np.uint8(m), mode='L')
+        m = m.resize(size, Image.NEAREST)
+
+        m = np.array(m)
+        m = np.array(m > 0).astype(np.uint8)
+        m = cv2.dilate(m,
+                       cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3)),
+                       iterations=4)
+        masks_expanded.append(Image.fromarray(m * 255))
+
+    return masks_expanded
+
+# def process_video(model, video, resolution=None):
+#     count = 0
+#     while video.isOpened() and count < 100:
+#         success, frame = video.read()
+#         if success:
+#             count += 1
+#             if resolution:
+#                 frame = cv2.resize(frame, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
+#             yield (frame, model(frame))
+#         else:
+#             break
+
+def get_segmented_frames(video_frames, model, resolution=None, human_filter=False):
+    if resolution:
+        frames = [cv2.resize(frame, dsize=(512, 512), interpolation=cv2.INTER_CUBIC) for frame in video_frames]
+    else:
+        frames = video_frames
+
+    frames_info = [model(frame) for frame in frames]
+
+    masks = []
+    for i in range(len(frames_info)):
+        if not human_filter:
+            masks.append(frames_info[i]['instances'].pred_masks)
+        else:
+            human_masks = []
+            for j, cls_id in enumerate(frames_info[i]['instances'].pred_classes):
+                if cls_id == 0:
+                    hmask = frames_info[i]['instances'].pred_masks[j]
+                    struct_element = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+                    hmask_processed = cv2.dilate(hmask.cpu().numpy().astype(np.uint8), struct_element, iterations=5).astype(np.bool)
+                    hmask_processed = torch.Tensor(hmask_processed).to(hmask.device)
+                    human_masks.append(hmask_processed)
+
+            if len(human_masks) == 0:
+                masks.append(torch.Tensor(human_masks))
+            else:
+                masks.append(torch.stack(human_masks))
+    
+    return frames, masks
+
+def inpaint(args, inpaint_model, segment_model, video_frames):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if args.model == "e2fgvi":
+        size = (432, 240)
+    elif args.set_size:
+        size = (args.width, args.height)
+    else:
+        size = None
+
+    # prepare datset
+    frames, masks = get_segmented_frames(video_frames, segment_model, human_filter=True)
+    frames = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), mode='RGB') for frame in frames]
+
+    frames, size = resize_frames(frames, size)
+    h, w = size[1], size[0]
+    video_length = len(frames)
+    imgs = to_tensors()(frames).unsqueeze(0) * 2 - 1
+    frames = [np.array(f).astype(np.uint8) for f in frames]
+
+    masks = process_masks(masks, size)
+    binary_masks = [
+        np.expand_dims((np.array(m) != 0).astype(np.uint8), 2) for m in masks
+    ]
+    masks = to_tensors()(masks).unsqueeze(0)
+    imgs, masks = imgs.to(device), masks.to(device)
+    comp_frames = [None] * video_length
+
+    # completing holes by e2fgvi
+    for f in tqdm(range(0, video_length, args.neighbor_stride)):
+        neighbor_ids = [
+            i for i in range(max(0, f - args.neighbor_stride),
+                             min(video_length, f + args.neighbor_stride + 1))
+        ]
+        ref_ids = get_ref_index(f, neighbor_ids, video_length, ref_length=args.step, num_ref=args.num_ref)
+        selected_imgs = imgs[:1, neighbor_ids + ref_ids, :, :, :]
+        selected_masks = masks[:1, neighbor_ids + ref_ids, :, :, :]
+        with torch.no_grad():
+            masked_imgs = selected_imgs * (1 - selected_masks)
+            mod_size_h = 60
+            mod_size_w = 108
+            h_pad = (mod_size_h - h % mod_size_h) % mod_size_h
+            w_pad = (mod_size_w - w % mod_size_w) % mod_size_w
+            masked_imgs = torch.cat(
+                [masked_imgs, torch.flip(masked_imgs, [3])],
+                3)[:, :, :, :h + h_pad, :]
+            masked_imgs = torch.cat(
+                [masked_imgs, torch.flip(masked_imgs, [4])],
+                4)[:, :, :, :, :w + w_pad]
+            pred_imgs, _ = inpaint_model(masked_imgs, len(neighbor_ids))
+            pred_imgs = pred_imgs[:, :, :h, :w]
+            pred_imgs = (pred_imgs + 1) / 2
+            pred_imgs = pred_imgs.cpu().permute(0, 2, 3, 1).numpy() * 255
+            for i in range(len(neighbor_ids)):
+                idx = neighbor_ids[i]
+                img = np.array(pred_imgs[i]).astype(
+                    np.uint8) * binary_masks[idx] + frames[idx] * (
+                        1 - binary_masks[idx])
+                if comp_frames[idx] is None:
+                    comp_frames[idx] = img
+                else:
+                    comp_frames[idx] = comp_frames[idx].astype(
+                        np.float32) * 0.5 + img.astype(np.float32) * 0.5
+    
+    ret_frames = []
+    for f in range(video_length):
+        comp = comp_frames[f].astype(np.uint8)
+        ret_frames.append(cv2.cvtColor(comp, cv2.COLOR_BGR2RGB))
+
+    return ret_frames
