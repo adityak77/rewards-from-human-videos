759beb772b216f4362f17ded9ecea9d4d86c0698
Aditya Kannan <akannan2@andrew.cmu.edu>
2022-12-05 15:44:38 -0500

    added inpainting with EgoHOS

--- dvd/inpaint_utils.py
+++ dvd/inpaint_utils.py
@@ -1,9 +1,11 @@
 import os
 import importlib
+import glob
 
 import numpy as np
 import cv2
 import torch
+from skimage.io import imsave
 
 from tqdm import tqdm
 from PIL import Image
@@ -15,6 +17,11 @@ from detectron2.engine import DefaultPredictor
 
 sys.path.append('/home/akannan2/inpainting/E2FGVI/')
 from core.utils import to_tensors
+
+sys.path.append('/home/akannan2/inpainting/EgoHOS/mmsegmentation/')
+from mmseg.apis import inference_segmentor, init_segmentor
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_human_cfg():
     config_file = '/home/akannan2/inpainting/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'
@@ -166,6 +173,126 @@ def inpaint(args, inpaint_model, segment_model, video_frames):
     # prepare datset
     frames, masks = get_segmented_frames(video_frames, segment_model, args.model, human_filter=True)
     frames = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), mode='RGB') for frame in frames]
+
+    frames, size = resize_frames(frames, size)
+    h, w = size[1], size[0]
+    video_length = len(frames)
+    imgs = to_tensors()(frames).unsqueeze(0) * 2 - 1
+    frames = [np.array(f).astype(np.uint8) for f in frames]
+
+    masks = process_masks(masks, size)
+    binary_masks = [
+        np.expand_dims((np.array(m) != 0).astype(np.uint8), 2) for m in masks
+    ]
+    masks = to_tensors()(masks).unsqueeze(0)
+    imgs, masks = imgs.to(device), masks.to(device)
+    comp_frames = [None] * video_length
+
+    # completing holes by e2fgvi
+    for f in tqdm(range(0, video_length, args.neighbor_stride)):
+        neighbor_ids = [
+            i for i in range(max(0, f - args.neighbor_stride),
+                             min(video_length, f + args.neighbor_stride + 1))
+        ]
+        ref_ids = get_ref_index(f, neighbor_ids, video_length, ref_length=args.step, num_ref=args.num_ref)
+        selected_imgs = imgs[:1, neighbor_ids + ref_ids, :, :, :]
+        selected_masks = masks[:1, neighbor_ids + ref_ids, :, :, :]
+        with torch.no_grad():
+            masked_imgs = selected_imgs * (1 - selected_masks)
+            mod_size_h = 60
+            mod_size_w = 108
+            h_pad = (mod_size_h - h % mod_size_h) % mod_size_h
+            w_pad = (mod_size_w - w % mod_size_w) % mod_size_w
+            masked_imgs = torch.cat(
+                [masked_imgs, torch.flip(masked_imgs, [3])],
+                3)[:, :, :, :h + h_pad, :]
+            masked_imgs = torch.cat(
+                [masked_imgs, torch.flip(masked_imgs, [4])],
+                4)[:, :, :, :, :w + w_pad]
+            pred_imgs, _ = inpaint_model(masked_imgs, len(neighbor_ids))
+            pred_imgs = pred_imgs[:, :, :h, :w]
+            pred_imgs = (pred_imgs + 1) / 2
+            pred_imgs = pred_imgs.cpu().permute(0, 2, 3, 1).numpy() * 255
+            for i in range(len(neighbor_ids)):
+                idx = neighbor_ids[i]
+                img = np.array(pred_imgs[i]).astype(
+                    np.uint8) * binary_masks[idx] + frames[idx] * (
+                        1 - binary_masks[idx])
+                if comp_frames[idx] is None:
+                    comp_frames[idx] = img
+                else:
+                    comp_frames[idx] = comp_frames[idx].astype(
+                        np.float32) * 0.5 + img.astype(np.float32) * 0.5
+    
+    ret_frames = []
+    for f in range(video_length):
+        comp = comp_frames[f].astype(np.uint8)
+        ret_frames.append(cv2.cvtColor(comp, cv2.COLOR_BGR2RGB))
+
+    return ret_frames
+
+
+"""         EGOHOS functions below         """
+def get_segmentation_model_egohos():
+    config_file = '/home/akannan2/inpainting/EgoHOS/mmsegmentation/work_dirs/seg_twohands_ccda/seg_twohands_ccda.py'
+    checkpoint_file = '/home/akannan2/inpainting/EgoHOS/mmsegmentation/work_dirs/seg_twohands_ccda/best_mIoU_iter_56000.pth'
+    model = init_segmentor(config_file, checkpoint_file, device=device)
+
+    return model
+
+def segment_video(reader, model, catchBadMasks=False):
+    height, width = reader[0].shape[0], reader[0].shape[1]
+    downsample_resolution_factor = max(width // 400 + 1, height // 400 + 1)
+    resolution = width // downsample_resolution_factor, height // downsample_resolution_factor
+    
+    video_dir = 'tmp/'
+    video_image_dir = os.path.join(video_dir, 'images')
+    os.makedirs(video_image_dir, exist_ok = True)
+    frames = []
+    for num, image in tqdm(enumerate(reader), total=len(reader)):
+        save_img_file = os.path.join(video_image_dir, str(num).zfill(8)+'.png')
+        image = cv2.resize(image, dsize=resolution, interpolation=cv2.INTER_CUBIC).astype(np.uint8)
+        frames.append(image)
+        imsave(save_img_file, image)
+
+    print('Segmenting video frames......')
+    masks = []
+    for file in tqdm(sorted(glob.glob(video_image_dir + '/*'))):
+        seg_result = inference_segmentor(model, file)[0]
+        masks.append(seg_result.astype(np.uint8))
+
+    masks = [(mask > 0).astype(np.uint8) for mask in masks]
+    dilate = lambda m : cv2.dilate(m, cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5)), iterations=5)
+    masks = [dilate(mask) for mask in masks]
+
+    os.system('rm -rf ' + video_dir)
+
+    # check criterion for if segmentation is good
+    if catchBadMasks:
+        zeroOrOne = lambda x : 1 if x > 0 else 0
+        mask_exists = [zeroOrOne(np.sum(mask)) for mask in masks]
+        if sum(mask_exists) / len(mask_exists) < 0.5:
+            print('Bad segmentation for video')
+
+    masks = [torch.from_numpy(mask) for mask in masks]
+
+    return frames, masks
+
+def inpaint_egohos(args, inpaint_model, segment_model, video_frames):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if args.model == "e2fgvi":
+        size = (432, 240)
+    elif args.set_size:
+        size = (args.width, args.height)
+    else:
+        size = None
+
+    # prepare datset
+    frames, masks = segment_video(video_frames, segment_model, catchBadMasks=True)
+    masks = [torch.tensor(mask, dtype=torch.float, device=device) for mask in masks]
+    masks = [mask.expand(3, -1, -1) for mask in masks]
+    frames = [Image.fromarray(frame, mode='RGB') for frame in frames]
 
     frames, size = resize_frames(frames, size)
     h, w = size[1], size[0]
