2e3610801535a2618fff84e4577b3a1db142b0f7
Aditya Kannan <akannan2@andrew.cmu.edu>
2023-01-23 18:48:28 -0500

    change batch cem plan to be default open loop

--- dvd/batch_cem_plan.sh
+++ dvd/batch_cem_plan.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run CEM planner with different environment seeds.
 
-NUM_SEEDS=5
+NUM_SEEDS=10
 
 for (( i=1; i<=$NUM_SEEDS; i++ ))
 do
-    python cem_plan_inpaint_egohos.py --seed $i "$@"
+    python cem_plan_open_loop.py --seed $i "$@"
 done
