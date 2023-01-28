b4db0467853f0675fc5b65310c55818ad026ed26
Aditya Kannan <akannan2@andrew.cmu.edu>
2023-01-27 19:15:07 -0500

    compiling CEM learned dynamics + code style to match current logging

--- dvd/cem_plan_visual_dynamics.py
+++ dvd/cem_plan_visual_dynamics.py
@@ -7,8 +7,8 @@ from torch import nn as nn
 import torch
 from torch.distributions.multivariate_normal import MultivariateNormal
 import time
-import copy
 import imageio
+import pickle
 
 import argparse
 import logging
@@ -16,13 +16,15 @@ import logging
 from sim_env.tabletop import Tabletop
 from optimizer_utils import CemLogger, decode_gif
 from optimizer_utils import load_discriminator_model, load_encoder_model, dvd_reward
-from optimizer_utils import reward_push_mug_left_to_right, reward_push_mug_forward, reward_close_drawer, tabletop_obs
+from optimizer_utils import tabletop_obs, get_success_values
 # from optimizer_utils import vip_reward, vip_reward_trajectory_similarity
 from visual_dynamics_model import load_world_model, rollout_trajectory
 
+from distutils.util import strtobool
+
 import sys
 sys.path.append('/home/akannan2/rewards-from-human-videos/pydreamer')
-from pydreamer.pydreamer import tools
+from pydreamer import tools
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG,
@@ -33,39 +35,67 @@ logging.getLogger('matplotlib.font_manager').disabled = True
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--task_id", type=int, required=True, help="Task to learn a model for")
+def get_args_conf():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task_id", type=int, required=True, help="Task to learn a model for")
+    parser.add_argument("--seed", type=int, default=-1, help="Random seed >= 0. If seed < 0, then use random seed")
+    parser.add_argument("--num_iter", type=int, default=100, help="Number of iterations of CEM")
 
-# optimizer specific
-parser.add_argument("--learn_dynamics_model", action='store_true', default=False, help='Learn a dynamics model (otherwise use online sampling)')
-parser.add_argument("--configs", nargs='+', required=True)
-parser.add_argument("--saved_model_path", type=str, default='../pydreamer/pydreamer/mlruns/0/tabletop/artifacts/checkpoints/latest.pt')
-parser.add_argument("--dvd", action='store_true', help='Use dvd rewards')
-parser.add_argument("--vip", action='store_true', help='Use pretrained VIP embeddings for reward function')
+    # learned visual dynamics model
+    parser.add_argument("--learn_dynamics_model", action='store_true', default=False, help='Learn a dynamics model (otherwise use online sampling)')
+    parser.add_argument("--configs", nargs='+', required=True)
+    parser.add_argument("--saved_model_path", type=str, default='/home/akannan2/tabletop_dynamics_old/artifacts/checkpoints/latest.pt')
 
-# for DVD or VIP rewards
-parser.add_argument("--demo_path", type=str, default=None, help='path to demo video')
+    # optimizer specific
+    parser.add_argument("--dvd", action='store_true', help='Use dvd rewards')
+    parser.add_argument("--vip", action='store_true', help='Use pretrained VIP embeddings for reward function')
 
-# dvd model params
-parser.add_argument("--checkpoint", type=str, default='test/tasks6_seed0_lr0.01_sim_pre_hum54144469394_dem60_rob54193/model/150sim_discriminator.pth.tar', help='path to model')
-parser.add_argument('--similarity', action='store_true', default=True, help='whether to use similarity discriminator') # needs to be true for MultiColumn init
-parser.add_argument('--hidden_size', type=int, default=512, help='latent encoding size')
-parser.add_argument('--num_tasks', type=int, default=2, help='number of tasks') # needs to exist for MultiColumn init
+    # for DVD or VIP rewards
+    parser.add_argument("--demo_path", type=str, default=None, help='path to demo video')
 
-# env initialization
-parser.add_argument("--log_dir", type=str, default="mppi")
-parser.add_argument("--env_log_freq", type=int, default=20) 
-parser.add_argument("--verbose", type=bool, default=1) 
-parser.add_argument("--xml", type=str, default='env1')
+    # dvd model params
+    parser.add_argument("--checkpoint", type=str, default='test/tasks6_seed0_lr0.01_sim_pre_hum54144469394_dem60_rob54193/model/150sim_discriminator.pth.tar', help='path to model')
+    parser.add_argument('--similarity', action='store_true', default=True, help='whether to use similarity discriminator') # needs to be true for MultiColumn init
+    parser.add_argument('--hidden_size', type=int, default=512, help='latent encoding size')
+    parser.add_argument('--num_tasks', type=int, default=2, help='number of tasks') # needs to exist for MultiColumn init
 
-args = parser.parse_args()
+    # env initialization
+    parser.add_argument("--log_dir", type=str, default="mppi")
+    parser.add_argument("--env_log_freq", type=int, default=20) 
+    parser.add_argument("--verbose", type=bool, default=1) 
+    parser.add_argument("--xml", type=str, default='env1')
+
+    args, remaining = parser.parse_known_args()
+
+    # Config from YAML
+    conf = {}
+    configs = tools.read_yamls('../pydreamer/config')
+    for name in args.configs:
+        if ',' in name:
+            for n in name.split(','):
+                conf.update(configs[n])
+        else:
+            conf.update(configs[name])
+
+    # Override config from command-line
+    parser = argparse.ArgumentParser()
+    for key, value in conf.items():
+        type_ = type(value) if value is not None else str
+        if type_ == bool:
+            type_ = lambda x: bool(strtobool(x))
+        parser.add_argument(f'--{key}', type=type_, default=value)
+    conf = parser.parse_args(remaining)
+
+    return args, conf
 
 
 if __name__ == '__main__':
+    args, conf = get_args_conf()
+
     TIMESTEPS = 51 # MPC lookahead - max length of episode
     N_SAMPLES = 100
     NUM_ELITES = 7
-    NUM_ITERATIONS = 1000
+    NUM_ITERATIONS = args.num_iter
     nu = 4
     nx = 13
 
@@ -75,7 +105,7 @@ if __name__ == '__main__':
     assert sum([args.dvd, args.vip]) == 1
 
     if args.learn_dynamics_model:
-        assert args.conf and args.saved_model_path
+        assert args.configs and args.saved_model_path
 
     # assigning reward functions
     if args.dvd:
@@ -93,17 +123,8 @@ if __name__ == '__main__':
         sim_discriminator = None
         terminal_reward_fn = vip_reward
 
-    # Config from YAML
-    conf = {}
-    configs = tools.read_yamls('./config')
-    for name in args.configs:
-        if ',' in name:
-            for n in name.split(','):
-                conf.update(configs[n])
-        else:
-            conf.update(configs[name])
-
     world_model = load_world_model(conf, args.saved_model_path)
+    init_image = imageio.imread('init_state.png')
 
     # reading demos
     if os.path.isdir(args.demo_path):
@@ -141,25 +162,27 @@ if __name__ == '__main__':
 
         tstart = time.perf_counter()
 
-        all_obs = np.zeros((1, TIMESTEPS+1, 120, 180, 3))
+        states = np.zeros((1, TIMESTEPS+1, 120, 180, 3))
+        states[0] = init_image
+        all_low_dim_states = np.zeros((TIMESTEPS, nx))
         actions = np.zeros((TIMESTEPS, nu))
 
         # if learned dynamics model then do closed loop
         if args.learn_dynamics_model:
             for t in range(TIMESTEPS):
-                if torch.matrix_rank(actions_cov) < actions_cov.shape[0]:
-                    actions_cov += 1e-5 * torch.eye(actions_cov.shape[0])
+                print(t)
+                tstart = time.time()
                 action_distribution = MultivariateNormal(actions_mean, actions_cov)
                 action_samples = action_distribution.sample((N_SAMPLES,))
                 sample_rewards = torch.zeros(N_SAMPLES)
 
-                ac_seqs = action_samples[:, t:]
+                ac_seqs = action_samples.reshape(N_SAMPLES, TIMESTEPS, nu)[:, t:]
                 assert (t + ac_seqs.shape[1]) == TIMESTEPS
-                init_states = all_obs[:, t].reshape(0, 3, 1, 2).tile(N_SAMPLES, 1, 1, 1)
+                init_states = np.tile(states[:, t].transpose(0, 3, 1, 2), (N_SAMPLES, 1, 1, 1))
                 obs_sampled = rollout_trajectory(init_states, ac_seqs, world_model) # shape B x (T-t) x H x W x C
 
-                prefix_obs = all_obs[:, :(t+1)].tile(N_SAMPLES, 1, 1, 1, 1)
-                obs_sampled = np.concatenate((prefix_obs, obs_sampled.detach().cpu().numpy()), axis=1) # shape B x (T+1) x H x W x C
+                prefix_obs = np.tile(states[:, :(t+1)], (N_SAMPLES, 1, 1, 1, 1))
+                obs_sampled = np.concatenate((prefix_obs, obs_sampled), axis=1) # shape B x (T+1) x H x W x C
 
                 sample_rewards = terminal_reward_fn(obs_sampled, _, demos=demos, video_encoder=video_encoder, sim_discriminator=sim_discriminator)
                 print(f'sample_rewards timestep {t}:', sample_rewards.min(), sample_rewards.mean(), sample_rewards.max())
@@ -179,9 +202,12 @@ if __name__ == '__main__':
 
                 action = traj_sample[0, t*nu:(t+1)*nu].cpu().numpy() # from CEM
                 obs, r, done, low_dim_info = env.step(action)
-                all_obs[0, t+1] = obs
+                states[0, t+1] = obs
+                all_low_dim_states[t] = tabletop_obs(low_dim_info)
 
                 actions[t, :] = action
+                tend = time.time()
+                print(f'timestep {t} took {tend-tstart} seconds')
 
         # run open loop CEM if online sampling - not ready yet, and probably not worth
         # integrating with open loop since we can run a separate script to do this.
@@ -190,40 +216,45 @@ if __name__ == '__main__':
             for t in range(TIMESTEPS):
                 action = traj_sample[0, t*nu:(t+1)*nu].cpu().numpy() # from CEM
                 obs, r, done, low_dim_info = env.step(action)
-                all_obs[0, t] = obs
+                states[0, t] = obs
 
         tend = time.perf_counter()
 
         # ALL CODE BELOW for logging sampled trajectory
         additional_reward_type = 'vip' if args.vip else 'dvd'
-        additional_reward = terminal_reward_fn(all_obs, _, demos=demos, video_encoder=video_encoder, sim_discriminator=sim_discriminator).item()
-        
-        low_dim_state = tabletop_obs(low_dim_info)
-        # TODO: fix reward section below / integrate with gt rewards above
-        if args.task_id == 94:
-            rew = low_dim_state[3] - very_start[3]
-            gt_reward = -np.abs(low_dim_state[3] - very_start[3] - 0.15) + 0.15
-            penalty = 0 if (np.abs(low_dim_state[10] - very_start[10]) < 0.03 and low_dim_state[12] < 0.01) else -100
-            success_threshold = 0.05
-        elif args.task_id == 41:
-            rew = low_dim_state[4] - very_start[4]
-            gt_reward = -np.abs(low_dim_state[4] - very_start[4] - 0.115) + 0.115
-            penalty = 0 # if (np.abs(low_dim_state[3] - very_start[3]) < 0.05) else -100
-            success_threshold = 0.03
-        elif args.task_id == 5:
-            rew = low_dim_state[10]
-            gt_reward = low_dim_state[10]
-            penalty = 0 if (np.abs(low_dim_state[3] - very_start[3]) < 0.01) else -100
-            success_threshold = -0.01
+        if not args.engineered_rewards:
+            additional_reward = terminal_reward_fn(states, _, demos=demos, video_encoder=video_encoder, sim_discriminator=sim_discriminator).item()
+        else:
+            additional_reward = 0 # NA
 
         # calculate success and reward of trajectory
-        gt_reward += penalty
-        succ = gt_reward > success_threshold
+        any_timestep_succ = False
+        for t in range(TIMESTEPS):
+            low_dim_state = all_low_dim_states[t]
+            _, gt_reward, succ = get_success_values(args.task_id, low_dim_state, very_start)
+            
+            any_timestep_succ = any_timestep_succ or succ
+
+        low_dim_state = tabletop_obs(low_dim_info)
+        rew, gt_reward, succ = get_success_values(args.task_id, low_dim_state, very_start)
+
         mean_sampled_rewards = sample_rewards.cpu().numpy().mean()
 
         logger.debug(f"{ep}: Trajectory time: {tend-tstart:.4f}\t Object Position Shift: {rew:.6f}")
-        cem_logger.update(gt_reward, succ, mean_sampled_rewards, additional_reward, additional_reward_type)
+        cem_logger.update(gt_reward, succ, any_timestep_succ, mean_sampled_rewards, additional_reward, additional_reward_type)
 
         # logging results
-        all_obs = (all_obs[0] * 255).astype(np.uint8)
+        all_obs = (states[0] * 255).astype(np.uint8)
         cem_logger.save_graphs(all_obs)
+
+        res_dict = {
+            'total_iterations': cem_logger.total_iterations,
+            'total_last_success': cem_logger.total_last_success,
+            'total_any_timestep_success': cem_logger.total_any_timestep_success,
+            'succ_rate_history': cem_logger.succ_rate_history,
+            'gt_reward_history': cem_logger.gt_reward_history,
+            'seed': args.seed,
+        }
+
+        with open(os.path.join(cem_logger.logdir, 'results.pkl'), 'wb') as f:
+            pickle.dump(res_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
--- dvd/visual_dynamics_model.py
+++ dvd/visual_dynamics_model.py
@@ -10,8 +10,8 @@ from torch import Tensor
 
 import sys
 sys.path.append('/home/akannan2/rewards-from-human-videos/pydreamer')
-from pydreamer.pydreamer.models.dreamer import Dreamer
-from pydreamer.pydreamer.models.functions import map_structure, flatten_batch
+from pydreamer.models.dreamer import Dreamer
+from pydreamer.models.functions import map_structure, flatten_batch
 
 TORCH_DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -23,56 +23,69 @@ def rollout_trajectory(init_state, ac_seqs, world_model):
 
     :return all_obs np.ndarray (B, T, H, W, C)
     """
-    B, W = init_state.shape[0], init_state.shape[-1]
+    B, C, H, W = init_state.shape
+    T = ac_seqs.shape[1]
     iwae_samples = 1
     do_open_loop = False
 
     ac_seqs = ac_seqs.float().to(TORCH_DEVICE)
-    init_state = torch.tensor(init_state, dtype=torch.float32).to(TORCH_DEVICE)
+    init_state = torch.tensor(init_state, dtype=torch.float32, device=TORCH_DEVICE)
+
+    # init_state tensor must be resized to 64x64
+    init_state = F.interpolate(init_state, size=(64, 64), mode='bilinear')
 
     all_obs = []
     all_features = []
     
-    obs = {'image': init_state.unsqueeze(0), 
-           'action' : torch.zeros((1, B, 4)),
-           'reset': torch.zeros((1, B), dtype=torch.bool),
-           'vecobs': torch.zeros((1, B, W))
+    obs = {'image': init_state.unsqueeze(0).repeat(T, 1, 1, 1, 1), 
+           'action' : torch.transpose(ac_seqs, 0, 1),
+           'reset': torch.zeros((T, B), dtype=torch.bool, device=TORCH_DEVICE),
           } # first input
-    in_state = (torch.zeros((B, 2048)), torch.zeros((B, 1024))) # first input
-    embed = world_model.encoder(obs)
+    in_state = world_model.init_state(B * iwae_samples) # first input
+    
+    with torch.no_grad():
+        embed = world_model.encoder(obs) # only obs['image'] used here
+        print('embed:', embed.size())
 
-    prior, post, post_samples, features, states, out_state = \
-        world_model.core.forward(embed,
-                                 obs['action'],
-                                 obs['reset'],
-                                 in_state,
-                                 iwae_samples=iwae_samples,
-                                 do_open_loop=do_open_loop)
+        print('obs[image]:', obs['image'].size())
+        print('obs[action]:', obs['action'].size())
+        print('obs[reset]:', obs['reset'].size())
+        prior, post, post_samples, features, states, out_state = \
+            world_model.core.forward(embed,
+                                    obs['action'],
+                                    obs['reset'],
+                                    in_state,
+                                    iwae_samples=iwae_samples,
+                                    do_open_loop=do_open_loop)
 
-    state = map_structure(states, lambda x: flatten_batch(x.detach())[0])
-    for t in range(ac_seqs.shape[0]):
+        state = map_structure(states, lambda x: x.detach()[0, :, 0])
+        for t in range(ac_seqs.shape[1]):
+            feature = world_model.core.to_feature(*state)
+            cur_ac = ac_seqs[:, t]
+            
+            all_features.append(feature)
+            _, state = world_model.core.cell.forward_prior(cur_ac, None, state)
+
         feature = world_model.core.to_feature(*state)
-        cur_ac = ac_seqs[:, t]
-        
         all_features.append(feature)
-        _, state = world_model.core.cell.forward_prior(cur_ac, None, state)
+        all_features = torch.stack(all_features)
 
-    feature = world_model.core.to_feature(*state)
-    all_features.append(feature)
-    all_features = torch.stack(all_features)
+        all_obs = world_model.decoder.image.forward(all_features)
 
-    all_obs = world_model.decoder.image.forward(all_features)
+    all_obs_flat = all_obs.reshape((T+1)*B, C, 64, 64)
+    all_obs = F.interpolate(all_obs_flat, size=(H, W), mode='bilinear')
+    all_obs = all_obs.reshape(T+1, B, C, H, W).detach().cpu().numpy()
 
-    return all_obs.reshape(0, 1, 3, 4, 2) # should be B x T x H x W x C
+    return all_obs.transpose(1, 0, 3, 4, 2) # should be B x T x H x W x C
 
 
 def load_world_model(conf, model_weights_path):
     assert conf.model == 'dreamer'
     model = Dreamer(conf)
 
-    model.load_state_dict(torch.load(model_weights_path))
-
-    return model.world_model
+    model.load_state_dict(torch.load(model_weights_path)['model_state_dict'])
+    world_model = model.wm
+    return world_model.to(TORCH_DEVICE).eval()
 
 
 '''
